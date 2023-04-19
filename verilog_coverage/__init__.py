77b7e655b66902d1695d49bbc9a3f5027aad2db2
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-04-19 12:32:34 -0400

    add pyslang dependency

--- verilog_coverage/__init__.py
+++ verilog_coverage/__init__.py
@@ -2,4 +2,30 @@
 # released under BSD 3-Clause License
 # author: Kevin Laeufer <laeufer@berkeley.edu>
 
+from pyslang import *
+
 __version__ = '0.1.0'
+
+
+test = """
+module test(input clk, input a, input b, output c);
+  always 
+endmodule
+"""
+
+
+def main():
+    print("Test")
+
+    tree = SyntaxTree.fromText(test)
+    print(tree)
+    comp = Compilation()
+    comp.addSyntaxTree(tree)
+    diags = comp.getAllDiagnostics()
+    report = DiagnosticEngine.reportAll(comp.sourceManager, diags)
+    print(report)
+
+
+
+if __name__ == '__main__':
+    main()
